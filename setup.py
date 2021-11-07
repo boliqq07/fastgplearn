f86c4975c31f228dad3d8d8cb463034c99c9f169
changxin W <986798607@qq.com>
2021-11-08 00:36:24 +0800

    first generate

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ from os import path
 from setuptools import setup, find_packages
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'fastgplearn/README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
 
     name='fastgplearn',
-    version='0.0.02',
+    version='0.0.03',
     keywords=['genetic programming","symbolic learning'],
     description='Fast fitting formula.',
     install_requires=['pandas', 'numpy', 'scipy', 'scikit-learn', 'joblib', 'deprecated',
@@ -28,17 +28,11 @@ setup(
     python_requires='>=3.6',
     url='https://github.com/boliqq07/fastgplearn',
     maintainer='wangchangxin',
-    platforms=[
-        "Windows",
-        "Linux",
-    ],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: Linux",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
